e77421e1cd8a2fb855684532e62f6662d1396ab3
AlexandreFleutelot <fleutelot.alexandre@gmail.com>
2023-03-06 17:39:04 +0100

    first  3d object

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,46 @@ use bevy::prelude::*;
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
+    .add_startup_system(setup)
     .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+
+    // tile
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.1, 0.1, 0.9).into()),
+        transform: Transform {
+            translation: Vec3 { x: 0.0, y: 0.1, z: 0.0 },
+            rotation: Quat::IDENTITY,
+            scale: Vec3 { x: 1.0, y: 0.2, z: 1.0 },
+        },
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }

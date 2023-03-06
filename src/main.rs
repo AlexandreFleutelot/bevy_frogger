19a77c9e2bb78bed54f4a3eed9f62d1f8a53a43a
AlexandreFleutelot <fleutelot.alexandre@gmail.com>
2023-03-06 23:48:51 +0100

    frog jump on key press

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,62 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Frog;
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
-    .add_startup_system(setup)
-    .run();
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
+        .add_startup_system(setup)
+        .add_startup_system(spawn_frog)
+        .add_system(frog_jump)
+        .run();
 }
+
+
 
 fn setup(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
-
-    // tile
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.1, 0.1, 0.9).into()),
-        transform: Transform {
-            translation: Vec3 { x: 0.0, y: 0.1, z: 0.0 },
-            rotation: Quat::IDENTITY,
-            scale: Vec3 { x: 1.0, y: 0.2, z: 1.0 },
-        },
-        ..default()
-    });
-    // light
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
-    // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+    commands.spawn(Camera2dBundle::default());
 }
+
+
+fn spawn_frog(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let frog_texture = asset_server.load("frog.png");
+
+    let texture_atlas_handle =  texture_atlases.add(TextureAtlas::from_grid(
+        frog_texture, 
+        Vec2::new(56.0, 80.0), 
+        7, 1, 
+        None, None));
+
+    commands.spawn(
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle.clone(),
+            ..default()
+        })
+        .insert(Frog)
+        .insert(AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)));
+}
+
+fn frog_jump(
+    kb: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut frog_query: Query<(&mut AnimationTimer, &mut TextureAtlasSprite), With<Frog>>,
+) {
+    for (mut timer, mut sprite) in &mut frog_query {
+        if sprite.index < 6 {
+            timer.tick(time.delta());
+            if timer.just_finished() {
+                sprite.index += 1;
+            }
+        }else if kb.just_pressed(KeyCode::Space) { sprite.index = 0 }
+    }
+}    

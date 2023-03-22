246d8e894463296f240d4877bc395f7f6b2df046
AlexandreFleutelot <fleutelot.alexandre@gmail.com>
2023-03-22 22:14:29 +0100

    add trunks

--- src/components.rs
+++ src/components.rs
@@ -5,3 +5,12 @@ pub struct Velocity(pub Vec3);
 
 #[derive(Component)]
 pub struct SpriteSize(pub Vec2);
+
+#[derive(Component)]
+pub struct Spawner {
+    pub sprite: Handle<Image>,
+    pub transform: Transform,
+    pub timer: Timer,
+    pub velocity: Vec3,
+    pub size: Vec2,
+}
